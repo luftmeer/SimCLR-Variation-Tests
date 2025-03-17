4cfb1a8b5af8537af098febc832921fd42e59ade
Alexander Schmiedel <alexander.schmiedel@gmail.com>
2025-03-17 13:31:24 +0100

    Removed match syntax to allow lower Python version to run

--- utils/dataset_loader.py
+++ utils/dataset_loader.py
@@ -10,44 +10,43 @@ def get_dataset(dataset_name: str='CIFAR10', train: bool=True, image_size: int=2
     transform = SimCLRTransform(size=image_size, n=augmentations)
     root_dir = './data'
     assert dataset_name in DATASETS
-    match dataset_name:
-        case 'CIFAR10':
-            ds = torchvision.datasets.CIFAR10(
-                root=root_dir,
-                train=train,
-                transform=transform,
-                download=True,
-            )
+    if dataset_name == 'CIFAR10':
+        ds = torchvision.datasets.CIFAR10(
+            root=root_dir,
+            train=train,
+            transform=transform,
+            download=True,
+        )
             
-        case 'STL10':
-            ds = torchvision.datasets.STL10(
-                root=root_dir,
-                split='unlabeled',
-                transform=transform,
-                download=True,
-            )
+    elif dataset_name == 'STL10':
+        ds = torchvision.datasets.STL10(
+            root=root_dir,
+            split='unlabeled',
+            transform=transform,
+            download=True,
+        )
             
-        case 'Imagenette':
-            ds = torchvision.datasets.Imagenette(
-                root=root_dir,
-                split='train',
-                size='full',
-                transform=transform,
-                download=True,
-            )
+    elif dataset_name == 'Imagenette':
+        ds = torchvision.datasets.Imagenette(
+            root=root_dir,
+            split='train',
+            size='full',
+            transform=transform,
+            download=True,
+        )
             
-        case 'tiny-imagenet':
-            raise NotImplementedError("TODO!")
-            if 'HF_TOKEN' in os.environ:
-                token = os.environ.get('HF_TOKEN')
-            elif 'HF_TOKEN' in kwargs.keys():
-                token = kwargs['HF_TOKEN']
-            else:
-                raise AttributeError(f"Token for Hugging Face not set or given.")
-            
-            user = huggingface_hub.login(token)
-            ds = load_dataset("zh-plus/tiny-imagenet")
-            ds.save_to_disk(root_dir)
+    elif dataset_name == 'tiny-imagenet':
+        raise NotImplementedError("TODO!")
+        if 'HF_TOKEN' in os.environ:
+            token = os.environ.get('HF_TOKEN')
+        elif 'HF_TOKEN' in kwargs.keys():
+            token = kwargs['HF_TOKEN']
+        else:
+            raise AttributeError(f"Token for Hugging Face not set or given.")
+        
+        user = huggingface_hub.login(token)
+        ds = load_dataset("zh-plus/tiny-imagenet")
+        ds.save_to_disk(root_dir)
             
     return ds
             
