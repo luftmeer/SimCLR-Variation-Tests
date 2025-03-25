f27dbfd92b4cb9bfeb6c6994e245208833f71fa9
Alexander Schmiedel <alexander.schmiedel@gmail.com>
2025-03-25 10:39:58 +0100

    Current State of Linear Evaluation. Added Changes to save: Distinguish between DDP and normal mode

---
+++ linear_evaluation.py
@@ -0,0 +1,97 @@
+import argparse
+import yaml
+
+import torch
+import torch.nn as nn
+
+from utils.dataset_loader import get_dataset
+from models.encoder import get_encoder
+from simclr.simclr import SimCLR
+from utils.loader import load_model, save_evaluation
+from torcheval.metrics import MulticlassAccuracy
+
+from tqdm import tqdm
+
+def train(simclr_model, model, optimizer, criterion, train_loader, device, args):
+    accuracy_epoch = MulticlassAccuracy(num_classes=args.n_classes)
+    loss_epoch = 0
+    for step, (img, target) in tqdm(enumerate(train_loader), desc='LE Training', total=len(train_loader)):
+        optimizer.zero_grad()
+        
+        img = img.to(device)
+        target = target.to(device)
+        
+        h, _ = simclr_model([img])
+        
+        out = model(h[0])
+        loss = criterion(out, target)
+        
+        accuracy_epoch.update(out, target)
+        loss_epoch += loss.item()
+        
+        loss.backward()
+        optimizer.step()
+    return loss_epoch, accuracy_epoch.compute()
+
+def main(args):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    # Dataset retrieval
+    train_dataset = get_dataset(dataset_name=args.dataset_name, train=True, image_size=args.resize, HF_TOKEN=args.HF_TOKEN, eval=True, args=args)
+    test_dataset = get_dataset(dataset_name=args.dataset_name, train=False, image_size=args.resize, HF_TOKEN=args.HF_TOKEN, eval=True, args=args)
+
+    # DataLoaders
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
+
+    # Encoder
+    encoder, n_features = get_encoder(encoder=args.encoder, widening=args.widening)
+    
+    # Model
+    simclr_model = SimCLR(encoder=encoder, n_features=n_features, projection_dim=args.projection_dim, image_size=args.resize, batch_size=args.batch_size, device=device).to(device)
+    simclr_model_state, cpt_epoch = load_model(path=args.checkpoint, device=device, eval=True)
+    simclr_model.load_state_dict(simclr_model_state)
+    simclr_model.eval() # Freeze Model
+    
+    # Classifier 
+    model = nn.Linear(n_features, args.n_classes)
+    model = model.to(device)
+    
+    # Optimizer & Criterion
+    optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
+    criterion = torch.nn.CrossEntropyLoss()
+    
+    # Train
+    for epoch in range(args.epochs_le):
+        loss_epoch, accuracy_epoch = train(simclr_model, model, optimizer, criterion, train_loader, device, args)
+
+        print(f"Epoch {epoch+1} | Loss: {loss_epoch} | Accuracy: {accuracy_epoch}")
+        
+    # Evaluate
+    
+    # Save Evaluation
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description="Linear Evaluation of SimCLR",
+    )
+    
+    parser.add_argument('--checkpoint', type=str)
+    
+    parser.add_argument('--config', type=str, default='./config/le/default.yaml')
+    
+    parser.add_argument('--slurm_job_id', type=int, default=None)
+    
+    # Parse arguments known up till here, the rest via config file
+    args = parser.parse_known_args()[0]
+    
+    with open(args.config, 'r') as file:
+        config = yaml.safe_load(file)
+        for elem in config:
+            k, v = elem.popitem()
+            parser.add_argument(f"--{k}", default=v, type=type(v))    
+
+    args = parser.parse_args()
+    
+    main(args)
--- utils/loader.py
+++ utils/loader.py
@@ -6,8 +6,16 @@ import argparse
 # Base Folder for Checkpoints
 CHECKPOINTS_FOLDER = './checkpoints'
 
-def load_model(path:str) -> torch.nn:
-    pass
+def load_model(path:str, device:torch.device, eval:bool=False) -> torch.nn:
+    if not os.path.exists(path):
+        raise FileNotFoundError("File or Path not correct")
+
+    cpt = torch.load(path, map_location=device.type, weights_only=False)
+    
+    if eval:
+        return cpt['model_state_dict'], cpt['epoch']
+    
+    return cpt
 
 
 def save_model(model: torch.nn.Module, optimizer: torch.optim.Optimizer, loss: object, dataset_name: str, epoch: int, encoder: str, args:argparse.Namespace) -> None:
@@ -18,6 +26,11 @@ def save_model(model: torch.nn.Module, optimizer: torch.optim.Optimizer, loss: o
     if not os.path.exists(f"{CHECKPOINTS_FOLDER}/{dataset_name}/"):
         os.makedirs(f"{CHECKPOINTS_FOLDER}/{dataset_name}/")
     
+    # Subfolder for Slurm Job
+    if args.slurm_job_id:
+        if not os.path.exists(f"{CHECKPOINTS_FOLDER}/{dataset_name}/{args.slurm_job_id}/"):
+            os.makedirs(f"{CHECKPOINTS_FOLDER}/{dataset_name}/{args.slurm_job_id}/")
+    
     filename_content = [args.encoder, 
                         args.optimizer, 
                         args.batch_size, 
@@ -27,9 +40,29 @@ def save_model(model: torch.nn.Module, optimizer: torch.optim.Optimizer, loss: o
                         epoch+1
                     ]
     
-    filename = f"{CHECKPOINTS_FOLDER}/{dataset_name}/{datetime.now().strftime('%Y%m%d%H%M%S')}-{args.slurm_job_id}_{'_'.join(str(elem) for elem in filename_content)}.cpt"
+    filename = f"{CHECKPOINTS_FOLDER}/{dataset_name}"
     
-    torch.save(
+    # Subfolder for Slurm Job
+    if args.slurm_job_id:
+        filename = filename + f"/{args.slurm_job_id}"
+    
+    filename = filename + f"/{datetime.now().strftime('%Y%m%d%H%M%S')}-{args.slurm_job_id}_{'_'.join(str(elem) for elem in filename_content)}.cpt"
+    
+    if isinstance(model, torch.nn.parallel.DistributedDataParallel):
+        torch.save(
+            {
+                "epoch": epoch + 1,
+                # +1 since, when starting again, the algorithm should continue with the next epoch and not 'redo' this one
+                "model_state_dict": model.module.state_dict(),
+                "optimizer": optimizer.state_dict(),
+                #"loss": loss.state_dict(),
+                "args": args,
+                "dataset_name": dataset_name,
+            },
+            filename,
+        )
+    else:
+        torch.save(
             {
                 "epoch": epoch + 1,
                 # +1 since, when starting again, the algorithm should continue with the next epoch and not 'redo' this one
@@ -38,6 +71,43 @@ def save_model(model: torch.nn.Module, optimizer: torch.optim.Optimizer, loss: o
                 #"loss": loss.state_dict(),
                 "args": args,
                 "dataset_name": dataset_name,
+            },
+            filename,
+        )
+    
+    return
+
+def save_evaluation(simclr_model: torch.nn.Module, model: torch.nn.Module, args: argparse.Namespace, cpt_epoch: int) -> None:
+    if not os.path.exists(CHECKPOINTS_FOLDER):
+        os.makedirs(CHECKPOINTS_FOLDER)
+
+    # Create sub folder for dataset name in checkpoint folder, if it doesn't exist yet
+    if not os.path.exists(f"{CHECKPOINTS_FOLDER}/{args.dataset_name}/"):
+        os.makedirs(f"{CHECKPOINTS_FOLDER}/{args.dataset_name}/")
+    
+    # Subfolder for Slurm Job
+    if args.slurm_job_id:
+        if not os.path.exists(f"{CHECKPOINTS_FOLDER}/{args.dataset_name}/{args.slurm_job_id}/"):
+            os.makedirs(f"{CHECKPOINTS_FOLDER}/{args.dataset_name}/{args.slurm_job_id}/")
+    
+    filename_content = [args.encoder,
+                        cpt_epoch
+                    ]
+    
+    filename = f"{CHECKPOINTS_FOLDER}/{args.dataset_name}"
+    
+    # Subfolder for Slurm Job
+    if args.slurm_job_id:
+        filename = filename + f"/{args.slurm_job_id}"
+    
+    filename = filename + f"/{datetime.now().strftime('%Y%m%d%H%M%S')}-{args.slurm_job_id}_{'_'.join(str(elem) for elem in filename_content)}.cpt"
+    
+    torch.save(
+            {
+                "simclr_model_state_dict": simclr_model.state_dict(),
+                "model_state_dict": model.state_dict(),
+                "args": args,
+                "dataset_name": args.dataset_name,
             },
             filename,
         )
