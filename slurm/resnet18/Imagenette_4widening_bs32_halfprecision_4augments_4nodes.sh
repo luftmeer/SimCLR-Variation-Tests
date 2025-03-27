bedf2ec723fad2e1a8942a65d68213ee35434118
Alexander Schmiedel <alexander.schmiedel@gmail.com>
2025-03-27 09:17:45 +0100

    Added ResNet50 Configs and Scripts + Added Configs and Scripts for 3 and 4 augmentated images

---
+++ slurm/resnet18/Imagenette_4widening_bs32_halfprecision_4augments_4nodes.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+#b Name and Files (also --job-name)
+#SBATCH -J simclr-multinode
+#Output and error (also --output, --error):
+#SBATCH -o ./%x.%j.out
+#SBATCH -e ./%x.%j.err
+#Initial working directory (also --chdir):
+#SBATCH -D ./
+#Notification and type
+#SBATCH --mail-type=NONE
+# Wall clock limit:
+#SBATCH --no-requeue
+
+#SBATCH --partition=NvidiaAll
+#Number of nodes and tasks per node:
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=4
+#SBATCh --cpus-per-task=4
+
+
+. ${PYENV_ROOT}/versions/aaml-simclr/bin/activate
+
+nodes=($(scontrol show hostnames $SLURM_JOB_NODELIST))
+nodes_array=($nodes)
+head_node=${nodes_array[1]}
+
+echo Node Ip: $head_node
+
+export LOGLEVEL_INFO
+export NCCL_DEBUG=INFO
+export NCCL_SOCKET_IFNAME=eth0
+
+srun torchrun --nnodes=4 --nproc_per_node=1 --rdzv_id=$RANDOM --rdzv_backend=c10d --rdzv_endpoint=$head_node:29603 train_ddp.py --slurm_job_id=$SLURM_JOB_ID --config=./config/resnet18/Imagenette_4widening_bs32_halfprecision_4augments_4nodes.yaml
