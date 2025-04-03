33f1d56b8cf163449fe18feab8007f2af763ca60
Alexander Schmiedel <alexander.schmiedel@gmail.com>
2025-04-03 11:31:42 +0200

    Add first epoch embedding to output

--- utils/logger.py
+++ utils/logger.py
@@ -185,7 +185,7 @@ class LinearEvaluationMonitor:
             self.log_model_weights(model, epoch)
             self.detect_anomalies(model, epoch)
 
-        if features is not None and labels is not None and (epoch+1 % 10 == 0 or epoch+1==101):
+        if features is not None and labels is not None and (epoch == 0 or epoch+1 % 10 == 0 or epoch+1==101):
             self.tb_writer.add_embedding(features, metadata=labels, tag=f"embeddings/epoch_{epoch}")
 
         if per_class_acc is not None:
