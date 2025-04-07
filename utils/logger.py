d9e08e710bfda6181c5bb38ee3d48592d40eaf63
Alexander Schmiedel <alexander.schmiedel@gmail.com>
2025-04-07 14:07:15 +0200

    Fix: type issue

--- utils/logger.py
+++ utils/logger.py
@@ -168,8 +168,8 @@ class TrainingMonitor:
                 labels = labels.detach().cpu().numpy()
 
             # Run t-SNE
-            tsne_e = TSNE(n_components=2, perplexity=30, init='pca', learning_rate='auto').fit_transform(embeddings[i])
-            tsne_p = TSNE(n_components=2, perplexity=30, init='pca', learning_rate='auto').fit_transform(projections[i])
+            tsne_e = TSNE(n_components=2, perplexity=30, init='pca', learning_rate='auto').fit_transform(embeddings[i].numpy())
+            tsne_p = TSNE(n_components=2, perplexity=30, init='pca', learning_rate='auto').fit_transform(projections[i].numpy())
 
             # Save plots
             self._save_tsne_plot(tsne_e, labels, f"tsne/tsne_embeddings_epoch{epoch}_augmentation{i}.png", title="t-SNE: Encoder Embeddings")
