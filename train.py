6ea61921ca6df190b2b16b0433318def3a395034
Alexander Schmiedel <alexander.schmiedel@gmail.com>
2025-04-21 17:04:53 +0200

    Converted DDP to single node script

--- train.py
+++ train.py
@@ -1,67 +1,113 @@
 import argparse
 
 from simclr.simclr import SimCLR
-from simclr.transform import SimCLRTransform
 from simclr.loss import NTXentLoss
 import torch
 from models.encoder import get_encoder
-from models import resnet, densenet
 import tqdm
 from utils.dataset_loader import get_dataset
 from utils import loader
 from utils.log_loss import log_loss
 import yaml
-from filelock import FileLock
-import socket
-import csv
 import time
-from torch.amp import autocast, GradScaler
-import os
 from itertools import combinations
+import datetime
+import math
+
+# DDP
+import torch.distributed as dist
+import os
 
 # LARS Optimizer
 from flash.core.optimizers import LARS
 
-def train(model, optimizer, loss_fn, train_loader, scaler, args):
+# Logging and Monitoring
+from utils.logger import TrainingMonitor
+
+# Base folder for all runs
+BASE_FOLDER = 'runs'
+
+def train(model, optimizer, loss_fn, train_loader, local_rank, global_rank, monitor, epoch, args):
+    all_embeddings = [[] for _ in range(args.augmentations)]
+    all_projections = [[] for _ in range(args.augmentations)]
+    all_labels = []
     total_loss = 0
-    for i, (augmentations, _) in tqdm.tqdm(enumerate(train_loader), desc="Training", total=len(train_loader)):
-        optimizer.zero_grad()
+    optimizer.zero_grad() # In case Gradient Accumulation is enabled and won't execute in first step
+    for step, (augmentations, labels) in tqdm.tqdm(enumerate(train_loader), desc="Training", total=len(train_loader)):
+        labels = labels.to(local_rank)
+        all_labels.append(labels.detach().cpu())
+        if args.ga and (step+1) % args.ga_count == 0 or not args.ga or (step+1) == len(train_loader):
+            optimizer.zero_grad()
         
+        hs, zs = model(augmentations)
+        #sim = torch.nn.functional.cosine_similarity(hs[0], hs[1]).mean()
+        #print(f'{sim=}')
+
         
-        with autocast(device_type='cuda'):
-            _, zs = model(augmentations)
-            zs = [z.float() for z in zs]
-            
-            loss = loss_fn(zs)
-        if torch.is_autocast_enabled():
-            scaler.scale(loss).backward()
-            if args.ga and i % args.ga_count == 0 or not args.ga or i+1 == len(train_loader):
-                scaler.step(optimizer)
-            scaler.update()
-        else:
-            
-            loss.backward()
+        # For logging purposes only
+        for i, h in enumerate(hs):
+            all_embeddings[i].append(h.detach().cpu())
+        
+        for i, z in enumerate(zs):
+            all_projections[i].append(z.detach().cpu())
+        
+        for comb_nr, (z_i, z_j) in enumerate(combinations(zs, 2)):
+            loss, logits, sim, positives, negatives = loss_fn(z_i, z_j)
+            full_loss = loss
+            total_loss += loss.item()
+            monitor.log_logits(i, epoch, comb_nr, logits)
+            #monitor.save_logits_softmax_plot(logits, epoch, step)
+            monitor.log_pos_neg_samples(positives, negatives, comb_nr)
+            monitor.log_losses(loss.item(), comb_nr)
+            if i % 50 == 0:
+                print(f"Step [{step}/{len(train_loader)}]\t Loss: {loss.item()} | Combination {comb_nr}")
+        
+        full_loss /= math.comb(args.augmentations, 2)
+        full_loss.backward()
+
+        if args.grad_clip:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+        
+        monitor.log_gradient(model, optimizer, i, epoch)
         
         # Gradient Accumulation
         # First Case: Gradient Accumulation is active and the n-th batch is rached which is divisible by ga_count
         # Second Case: Gradient Accumulation is not available -> always do the step
         # Third Case: The current batch is the last one -> always optimize
-            if args.ga and i % args.ga_count == 0 or not args.ga or i+1 == len(train_loader):
-                optimizer.step()
+        if args.ga and (step+1) % args.ga_count == 0 or not args.ga or (step+1) == len(train_loader):
+            optimizer.step()
         
-        
-        total_loss += loss.item()
-        
-        if i % 50 == 0:
-            print(f"Step [{i}/{len(train_loader)}]\t Loss: {loss.item()}")
 
-    return total_loss
+    return total_loss / len(train_loader), sim, all_embeddings, all_projections, all_labels
+
 
 def main(args):
+    cpt = None
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # If resuming -> overwrite config with "old" one. Keep checkpoint and resume value
+    if args.resume:
+        cpt_path = args.checkpoint
+        
+        cpt = loader.load_model(path=args.checkpoint, device=device)
+        args = cpt['args']
+        args.checkpoint = cpt_path
+        args.resume = True # Reset -> in previous it was False
+
     # Randomness
     torch.manual_seed(args.seed)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        
+    
+    # Monitoring
+    monitor = TrainingMonitor(
+        save_dir=f'{BASE_FOLDER}/{args.slurm_job_id}/',
+        plot_every=100,
+        batch_size=args.batch_size * args.ga_count if args.ga else 1,
+        maxlen=500,
+        enabled=True,
+        rank=0,
+        n_augments=args.augmentations,
+    )
+    
+    
     train_dataset = get_dataset(dataset_name=args.dataset_name, train=args.dataset_train, image_size=args.resize, augmentations=args.augmentations, HF_TOKEN=args.HF_TOKEN, args=args)
 
     train_loader = torch.utils.data.DataLoader(
@@ -70,65 +116,70 @@ def main(args):
             shuffle=True,
             drop_last=True,
             pin_memory=True,
+            sampler=None,
         )
 
     encoder, n_features = get_encoder(encoder=args.encoder, widening=args.widening)
-    model = SimCLR(encoder=encoder, n_features=n_features, projection_dim=args.projection_dim, image_size=args.resize, batch_size=args.batch_size, device=device).to(device)
 
+    model = SimCLR(encoder=encoder, n_features=n_features, projection_dim=args.projection_dim, image_size=args.resize, batch_size=args.batch_size, device=device).to(device)
+    
     if args.optimizer == 'Adam':
         optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
         scheduler = None
     elif args.optimizer == 'LARS':
-        optimizer = LARS(model.parameters(), lr=0.3*(args.batch_size/256), weight_decay=1e-6)
+        batch_size = args.batch_size
+        if dist.is_initialized():
+            batch_size *= dist.get_world_size()
+        
+        if args.ga:
+            batch_size *= args.ga_count
+            
+        optimizer = LARS(model.parameters(), lr=0.3*(batch_size/256), weight_decay=1e-6)
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epochs, eta_min=0, last_epoch=-1)
     
     if args.resume:
-        cpt = loader.load_model(path=args.checkpoint, device=device, eval=False)
         start_epoch = cpt['epoch']
         model.load_state_dict(cpt['model_state_dict'])
         optimizer.load_state_dict(cpt['optimizer'])
-            
+        
     else:
         start_epoch = 0
     
-    loss_fn = NTXentLoss(batch_size=args.batch_size, device=device).to(device)
+    loss_fn = NTXentLoss(batch_size=args.batch_size*dist.get_world_size(), device=device).to(device)
 
     model.to(device)
     
-        
-    if args.half_precision:
-        scaler = GradScaler()
-    else:
-            scaler = None
 
     if args.debug:
         # Anomaly detection -> In case of NaN resulting from the loss function
         torch.autograd.set_detect_anomaly(True)
 
-    model.train()
-    for epoch in range(start_epoch, args.epochs):
-        
+    model.train()    
+    for epoch in range(start_epoch, args.epochs):        
         start = time.time()
         
-        loss_epoch = train(model, optimizer, loss_fn, train_loader, scaler, args)
+        loss_epoch, sim, all_embeddings, all_projections, all_labels = train(model, optimizer, loss_fn, train_loader, device, 0, monitor, epoch, args)
         
         end = time.time()
         
         print(f'Epoch {epoch+1} | Loss: {loss_epoch}')
         
+        monitor.log_epoch(epoch, optimizer.param_groups[0]["lr"], end-start, sim)
+        monitor.log_tsne_embeddings(all_embeddings, all_projections, all_labels, epoch)
+        #monitor.check_embedding_collapse(all_embeddings, epoch)
+        
         if scheduler:
             scheduler.step()
         
         if args.metrics:
-            log_loss(epoch=epoch, loss=loss_epoch, args=args, elapsed_time=end-start)
+            log_loss(epoch=epoch, loss=loss_epoch, args=args, elapsed_time=end-start, base_folder=BASE_FOLDER)
             
         if (epoch+1) % args.save_every_epoch == 0:
             print(f"Saving model at Epoch {epoch+1}")
-            loader.save_model(model=model, optimizer=optimizer, loss=loss_fn, dataset_name=args.dataset_name, epoch=epoch, encoder=args.encoder, args=args)
+            loader.save_model(model=model, optimizer=optimizer, dataset_name=args.dataset_name, epoch=epoch, encoder=args.encoder, args=args, base_folder=BASE_FOLDER)
     
     print(f"Saving final model at Epoch {epoch+1}")
-    loader.save_model(model=model, optimizer=optimizer, loss=loss_fn, dataset_name=args.dataset_name, epoch=epoch, encoder=args.encoder, args=args)
-
+    loader.save_model(model=model, optimizer=optimizer, dataset_name=args.dataset_name, epoch=epoch+1, encoder=args.encoder, args=args, base_folder=BASE_FOLDER)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -144,6 +195,10 @@ if __name__ == '__main__':
     
     parser.add_argument('--debug', action='store_true')
     
+    parser.add_argument('--resume', action='store_true')
+    
+    parser.add_argument('--ga', action='store_true')
+    
     # Parse arguments known up till here, the rest via config file
     args = parser.parse_known_args()[0]
     
@@ -151,7 +206,12 @@ if __name__ == '__main__':
         config = yaml.safe_load(file)
         for elem in config:
             k, v = elem.popitem()
-            parser.add_argument(f"--{k}", default=v, type=type(v))    
+            if k in ['lr', 'weight_decay', 'eps']:
+                parser.add_argument(f'--{k}', default=v, type=float)
+            elif k == 'ga':
+                continue
+            else:
+                parser.add_argument(f"--{k}", default=v, type=type(v))    
 
     args = parser.parse_args()
     
